864497c3764f11aca7f91eb80d959fe4c687411c
Jessica Forchione <jsforchione@gmail.com>
2016-09-29 10:25:15 -0700

    update controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+get '/users' do 
+  @users = User.all 
+  erb :'users/index'
+end 
+
+get '/users/new' do 
+  erb :'users/new'
+end 
+
+get '/users/:id/edit' do 
+  @user = User.find(params[:id])
+  erb :'users/edit'
+end 
+
+get '/users/:id' do 
+  @user = User.find(params[:id])
+  redirect '/sessions/new' unless current_user
+  redirect '/questions' unless logged_in?
+  erb :'/users/show'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user] = @user
+    redirect "/users/#{@user.id}"
+  else 
+    @errors = ["Username or Email already exists"]
+    erb :'users/new'
+  end
+end
+
+put '/users/:id' do
+  @user = User.find(params[:id])
+  if params[:password] == ""
+    @user.update(email: params[:email], password_hash: @user.password_hash)
+  else
+    @user.update(email: params[:email], password: params[:password])
+  end
+  redirect "/users/#{@user.id}"
+end
+
